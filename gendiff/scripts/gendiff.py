dcb23feab3b2ca0617974e52c2f1e4b06f93aedc
SvamiBog <rubanezmaxim@gmailcom>
2024-02-07 19:53:39 +0100

    update code

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python
-import argparse
 import json
+from gendiff.cli import parse
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument(
-        '-f',
-        '--format',
-        help='set format of output',
-        default='stylish'
-    )
-
-    args = parser.parse_args()
+    args = parse()
 
     diff = generate_diff(args.first_file, args.second_file)
     print(diff)
