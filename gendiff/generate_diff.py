5e1824c8eddfa2aab46a2e83bf986b14c6b47685
SvamiBog <rubanezmaxim@gmailcom>
2024-02-25 19:00:59 +0100

    fixed file generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -9,7 +9,7 @@ def generate_diff(
         file2_path: str,
         formater: str = 'stylish'
 ) -> str:
-    date_fale1 = detect_file_format(file1_path)
-    date_fale2 = detect_file_format(file2_path)
-    diff = compare_config_files(date_fale1, date_fale2)
+    date_file1 = detect_file_format(file1_path)
+    date_file2 = detect_file_format(file2_path)
+    diff = compare_config_files(date_file1, date_file2)
     return get_formatter(formater)(diff)
