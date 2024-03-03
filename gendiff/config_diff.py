7d7030d1726082b87e0c177a1af72a495f158760
SvamiBog <rubanezmaxim@gmailcom>
2024-03-03 09:56:36 +0100

    refactoring code config_diff.py

--- gendiff/config_diff.py
+++ gendiff/config_diff.py
@@ -1,42 +1,31 @@
 #!/usr/bin/env python
 
 
-def compare_values(key, value1, value2):
-    if value1 == value2:
-        return {
-            'key': key,
-            'operation': 'same',
-            'value': value1
-        }
-    elif isinstance(value1, dict) and isinstance(value2, dict):
-        return {
-            'key': key,
-            'operation': 'nested',
-            'value': compare_config_files(value1, value2)
-        }
-    else:
-        return {
-            'key': key,
-            'operation': 'changed',
-            'old': value1,
-            'new': value2
-        }
-
-
-def handle_key(key, data1, data2):
-    if key in data1 and key in data2:
-        return compare_values(key, data1[key], data2[key])
-    elif key not in data1:
-        return {'key': key, 'operation': 'add', 'new': data2[key]}
-    else:
-        return {'key': key, 'operation': 'removed', 'old': data1[key]}
-
-
 def compare_config_files(data1: dict, data2: dict):
     diff = []
     all_keys = sorted(set(data1) | set(data2))
 
     for key in all_keys:
-        diff.append(handle_key(key, data1, data2))
+        if key in data1 and key in data2:
+            if data1[key] == data2[key]:
+                diff.append(
+                    {'key': key, 'operation': 'same', 'value': data1[key]}
+                )
+            elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+                diff.append(
+                    {'key': key,
+                     'operation': 'nested',
+                     'value': compare_config_files(data1[key], data2[key])}
+                )
+            else:
+                diff.append(
+                    {'key': key,
+                     'operation': 'changed',
+                     'old': data1[key], 'new': data2[key]}
+                )
+        elif key not in data1:
+            diff.append({'key': key, 'operation': 'add', 'new': data2[key]})
+        else:  # key not in data2
+            diff.append({'key': key, 'operation': 'removed', 'old': data1[key]})
 
     return diff
